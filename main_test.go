5ec7865138d3ea2899d1a4e492d1dc45cd814bd3
Verveyko Alexander <averveiko@gmail.com>
2021-09-14 16:11:54 +0300

    add table-driven test example

--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGetWordCount(t *testing.T) {
 	got := getWordCount("one two three")
@@ -15,5 +18,28 @@ func TestGetWordCountEmpty(t *testing.T) {
 	want := 0
 	if got != want {
 		t.Errorf("got %d; want %d", got, want)
+	}
+}
+
+// Пример табличного теста (table-driven)
+func TestGetWordCountTD(t *testing.T) {
+	var tests = []struct {
+		// name string Имя теста можно задавать явно или генерировать - как показано ниже
+		text string
+		want int
+	}{
+		{"word", 1},
+		{"", 0},
+		{"many many words and symbol -", 6},
+	}
+
+	for _, test := range tests {
+		name := fmt.Sprintf("case(%s)", test.text)
+		t.Run(name, func(t *testing.T) {
+			got := getWordCount(test.text)
+			if got != test.want {
+				t.Errorf("got %d, want %d", got, test.want)
+			}
+		})
 	}
 }
