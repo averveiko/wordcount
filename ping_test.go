e970d90a34ec8faf8a98316094e11ee3b19721b8
Verveyko Alexander <averveiko@gmail.com>
2021-09-16 14:30:55 +0300

    Пропуск теста в short режиме, setup и teardown

--- ping_test.go
+++ ping_test.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 )
 
 // Этот тест делает реальные запросы в сеть, что довольно долго (почти секунду)
 func TestPing(t *testing.T) {
+
+	// Этот тест будет пропущен в коротком режиме: $ go test -v -short (так как он слишком долгий)
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+
 	client := &http.Client{}
 	pinger := Pinger{client}
 	got := pinger.Ping("https://example.com")
@@ -56,3 +64,15 @@ func TestPingMocked(t *testing.T) {
 // Вообще, для http-заглушек в Go есть отдельный пакет net/http/httptest. Здесь общий пример общего подхода:
 // 	- Заменяем конкретную зависимость на зависимость от интерфейса.
 // 	- Реализуем интерфейс в тестах и используем его.
+
+// setup и teardown (например подготовка данных перед тестами и очистка после)
+func TestMain(m *testing.M) {
+	fmt.Println("Setup tests...")
+	start := time.Now()
+
+	m.Run()
+
+	fmt.Println("Teardown tests...")
+	end := time.Now()
+	fmt.Println("Tests took", end.Sub(start))
+}
